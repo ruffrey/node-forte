ba6682ff45627128b537d9bb942c6ada6913714a
Jeff Parrish <jeffhparrish@gmail.com>
2015-03-18 16:43:30 -0700

    refactor to unit tests to match new lib internals.

--- test/unit/api.spec.js
+++ test/unit/api.spec.js
@@ -3,62 +3,38 @@ var should = require('should');
 var Forte = require('../../index');
 var forte;
 
-describe('Unit', function () {
+describe('Unit tests', function () {
     beforeEach(function () {
-        forte = new Forte();
+        forte = new Forte({
+            "apiLoginId": "",
+            "secureTransactionKey": "",
+            "account_id": "",
+            "location_id": ""
+        });
         forte.setDevmode();
-        forte.setAuthHeader('asdf');
-        forte.setBasicAuth('username', 'password');
     });
 
-    it('has expected properties and methods', function () {
+    it('has expected base properties and methods', function () {
         forte._request.should.be.type('function');
         forte.setDevmode.should.be.type('function');
-        forte.setAuthHeader.should.be.type('function');
-        forte.setBasicAuth.should.be.type('function');
         forte._base.should.be.type('string');
-        forte._authHeader.should.be.type('string');
+        forte.location_id.should.be.type('string');
+        forte.account_id.should.be.type('string');
     });
 
     it('supports multiple instances', function () {
-        var a = new Forte();
-        a.setBasicAuth('user_a', 'pass_b');
-        var b = new Forte();
-        b.setBasicAuth('user_b', 'pass_b');
+        var a = new Forte({
+            apiLoginId: 'user_a'
+        });
+        var b = new Forte({
+            apiLoginId: 'user_b'
+        });
         b.setDevmode();
-        b._basicAuth.should.not.equal(a._basicAuth);
-        a._basicAuth.username.should.equal('user_a');
-        b._basicAuth.username.should.equal('user_b');
+
+        b.basicAuth.username.should.not.equal(a.basicAuth.username);
+        a.basicAuth.username.should.equal('user_a');
+        b.basicAuth.username.should.equal('user_b');
         b._base.should.not.equal(a._base);
-    });
-
-    it('supports chaining', function () {
-        var f = new Forte()
-                .setDevmode()
-                .setAuthHeader('hh')
-                .setBasicAuth('usr', 'pwd');
-        f._base.should.equal('https://sandbox.forte.net/api/v1');
-        f._authHeader.should.equal('hh');
-        f._basicAuth.should.eql({
-            username: 'usr',
-            password: 'pwd'
-        });
-    });
-
-    describe('setAuthHeader', function () {
-        it('should work', function () {
-            forte.setAuthHeader('asdfjkl');
-            forte._authHeader.should.equal('asdfjkl');
-        });
-    });
-
-    describe('setBasicAuth', function () {
-        it('should work', function () {
-            forte.setBasicAuth('username', 'password');
-            forte._basicAuth.should.be.type('object');
-            forte._basicAuth.username.should.equal('username');
-            forte._basicAuth.password.should.equal('password');
-        });
     });
 
 });
